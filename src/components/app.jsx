e1d6bc40e56dbdae72d4613862dd0499b664f9a9
diazproduction <diazproduction@hotmail.es>
2016-01-27 23:58:23 +0000

    Add custom theme for Material-UI.

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,7 +2,20 @@ import React from 'react';
 import MessagesList from './MessagesList.jsx';
 import AppBar from 'material-ui/lib/app-bar';
 
+import ThemeManager from 'material-ui/lib/styles/theme-manager';
+import MyRawTheme from '../theme/theme.jsx';
+
 var App = React.createClass({
+
+  childContextTypes : {
+    muiTheme: React.PropTypes.object,
+  },
+
+  getChildContext() {
+    return {
+      muiTheme: ThemeManager.getMuiTheme(MyRawTheme),
+    };
+  },
 
   render() {
     return(
---
+++ src/theme/theme.jsx
@@ -0,0 +1,18 @@
+import Colors from 'material-ui/lib/styles/colors';
+import ColorManipulator from 'material-ui/lib/utils/color-manipulator';
+import Spacing from 'material-ui/lib/styles/spacing';
+import zIndex from 'material-ui/lib/styles/zIndex';
+
+export default {
+  spacing: Spacing,
+  zIndex: zIndex,
+  fontFamily: 'Roboto, sans-serif',
+  palette: {
+    primary1Color: Colors.green500,
+    primary2Color: Colors.green700,
+    primary3Color: Colors.green200,
+    accent1Color: Colors.pinkA200,
+    accent2Color: Colors.grey100,
+    accent3Color: Colors.grey500
+  }
+}
