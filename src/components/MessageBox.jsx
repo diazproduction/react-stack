c643707bee77ddf678ec84594775cc6733efd790
diazproduction <diazproduction@hotmail.es>
2016-01-30 22:49:06 +0000

    Save new messages to Firebase.

--- src/components/MessageBox.jsx
+++ src/components/MessageBox.jsx
@@ -1,11 +1,47 @@
 import React from 'react';
+import trim from 'trim';
 import Card from 'material-ui/lib/card/card';
+import Firebase from 'firebase';
 
 var MessageBox = React.createClass({
+  getInitialState(){
+    return{
+      message: ""
+    }
+  },
+
+  componentWillMount(){
+    this.firebaseRef = new Firebase("https://glaring-heat-810.firebaseio.com/messages");
+  },
+
+  onChange(evt) {
+    this.setState({
+      message: evt.target.value
+    });
+  },
+
+  onKeyUp(evt){
+    if(evt.keyCode === 13 && trim(evt.target.value) != "") {
+      evt.preventDefault();
+
+      this.firebaseRef.push({
+        message: this.state.message
+      });
+
+      this.setState({
+        message: ""
+      });
+    }
+  },
+
   render() {
     return(
       <Card className="c-message-box">
-        <textarea className="c-message-box__textarea"/>
+        <textarea
+          value={this.state.message}
+          onChange={this.onChange}
+          onKeyUp={this.onKeyUp}
+          className="c-message-box__textarea"/>
       </Card>
     );
   }
--- src/components/MessagesList.jsx
+++ src/components/MessagesList.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Firebase from 'firebase';
+import {keys, map, clone} from 'lodash';
 
 import Message from './Message.jsx';
 import Card from 'material-ui/lib/card/card';
@@ -15,7 +16,14 @@ var MessagesList = React.createClass({
   componentWillMount() {
     this.firebaseRef = new Firebase("https://glaring-heat-810.firebaseio.com/messages");
     this.firebaseRef.once("value", (dataSnapshot)=> {
-      var messages = dataSnapshot.val();
+      var messagesVal = dataSnapshot.val();
+      var messages = _(messagesVal)
+        .keys()
+        .map((messagesKey) => {
+          var cloned = _.clone(messagesVal[messagesKey]);
+          cloned.key = messagesKey;
+          return cloned;
+        }).value();
       this.setState({
         messages: messages
       });
